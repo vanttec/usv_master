13475c09b1e2710079dfffe42d38a00dad799cee
pedrosc97 <pedro.sc.97@gmail.com>
2019-12-17 19:46:42 -0600

    Started work on USV Master Node

---
+++ scripts/usv_master.py
@@ -0,0 +1,7 @@
+import rospy
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Pose2D
+
+class USVMaster:
+    def __init__(self):
+        self.dataframe_pub = rospy.Publisher('/vanttec_usv/usv_master/usv_data', String, queue_size=10)
