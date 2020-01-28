a197a2ce2d1a8b810637b1b3870cd88eef1a67e6
Robertomendivil97 <robertomc97@gmail.com>
2020-01-27 19:48:56 -0600

    Usv master node refactor

--- scripts/usv_master.py
+++ scripts/usv_master.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
+import subprocess
+
 import rospy
-from std_msgs.msg import String
 from std_msgs.msg import Bool
 from std_msgs.msg import Int32
-from geometry_msgs.msg import Vector3
+from std_msgs.msg import String
 from geometry_msgs.msg import Pose2D
-import subprocess
+from geometry_msgs.msg import Vector3
+
 
 #TO DO 
 #Cambiar tipo de dato STATUS
-#Dejas
+#AUTONAV Y SPEED CHALLENGE
+#3 CURSOS Y 6 RETOS 
 
 
 
@@ -22,17 +25,17 @@ class USVMaster:
         self.start_mission = False
         self.stop_mission = False
 
-        #Data Input Status
+        #ROS Subscribers
         rospy.Subscriber("/usv_comms/boat_transceiver/course_config", String, self.mission_callback) 
         rospy.Subscriber("/mission_status", Int32, self.mission_status_callback)
         rospy.Subscriber("/usv_comms/boat_transceiver/stop_mission", Bool, self.stop_mission_callback)
         rospy.Subscriber("/usv_comms/boat_transceiver/start_mission", Bool , self.start_mission_callback)
 
-        ##Para mandar el status de las misiones
-
+        ##Ros Publishers
         self.dataframe_pub = rospy.Publisher('/vanttec_usv/usv_master/usv_data', String, queue_size=10)
         self.boat_current_mission_pub = rospy.Publisher('/usv_master/usv_master/general_status', String, queue_size=10) #<<--CHECK-->>#
     
+    #Callbacks
     def mission_callback(self,xbee_message):
         self.xbee_message = xbee_message.data
         rospy.loginfo(self.xbee_message)
@@ -49,44 +52,39 @@ class USVMaster:
 def main():
     rospy.init_node('usv_master', anonymous=False)
     rate = rospy.Rate(100)
-    usv_master = USVMaster()
+    usvMaster = USVMaster()
 
     #CUrrentMission Node 
     current_node=None 
     
     while not rospy.is_shutdown():    
-            #COURSE MESSAGES
-        #rospy.loginfo("mensaje leido = " + usv_master.xbee_message)
       
-        if (usv_master.xbee_message)== "CA" or (usv_master.xbee_message)== "ca" :
-            usv_master.boat_current_mission_pub.publish("Course Alpha")
+        if (usvMaster.xbee_message)== "CA" or (usvMaster.xbee_message)== "ca" :
+            usvMaster.boat_current_mission_pub.publish("Course Alpha")
 
-        elif (usv_master.xbee_message)== "CB" or (usv_master.xbee_message)== "cb":
-            usv_master.boat_current_mission_pub.publish("Course Beta")
+        elif (usvMaster.xbee_message)== "CB" or (usvMaster.xbee_message)== "cb":
+            usvMaster.boat_current_mission_pub.publish("Course Beta")
             
-        elif (usv_master.xbee_message)== "CC" or  (usv_master.xbee_message)== "cc":
-            usv_master.boat_current_mission_pub.publish("Course Charlie")
+        elif (usvMaster.xbee_message)== "CC" or  (usvMaster.xbee_message)== "cc":
+            usvMaster.boat_current_mission_pub.publish("Course Charlie")
             
-            #MISSION MESSAGES
-
-        elif (usv_master.xbee_message)== "AN" or  (usv_master.xbee_message)== "an":
+        elif (usvMaster.xbee_message)== "AN" or  (usvMaster.xbee_message)== "an":
             current_node = subprocess.Popen("rosrun rb_missions auto_nav_position.py", shell = True)
-            usv_master.xbee_message= ""
-            print(usv_master.xbee_message)
-            while usv_master.mission_status != 1 and  usv_master.stop_mission!=True and not rospy.is_shutdown():
-                 usv_master.boat_current_mission_pub.publish("Atonomous Navigation")
+            usvMaster.xbee_message= ""
+            print(usvMaster.xbee_message)
+            while usvMaster.mission_status != 1 and  usvMaster.stop_mission!=True and not rospy.is_shutdown():
+                 usvMaster.boat_current_mission_pub.publish("Atonomous Navigation")
                  rate.sleep()
             current_node.kill()
-                #wait             
-        elif (usv_master.xbee_message)== "SP" or (usv_master.xbee_message)== "sp":
+                         
+        elif (usvMaster.xbee_message)== "SP" or (usvMaster.xbee_message)== "sp":
             current_node = subprocess.Popen("rosrun rb_missions speed_ch.py", shell = True)
-            usv_master.xbee_message= ""
-            while usv_master.mission_status != 1 and usv_master.stop_mission!=True and not rospy.is_shutdown():
-                #wait
-                 usv_master.boat_current_mission_pub.publish("Speed Challenge")
+            usvMaster.xbee_message= ""
+            while usvMaster.mission_status != 1 and usvMaster.stop_mission!=True and not rospy.is_shutdown():
+                 usvMaster.boat_current_mission_pub.publish("Speed Challenge")
                  rate.sleep()
             current_node.kill()
-        usv_master.xbee_message= ""
+        usvMaster.xbee_message= ""
         rate.sleep()
 
     if current_node is not None:
@@ -98,21 +96,4 @@ if __name__ == "__main__":
         main()
     except rospy.ROSInterruptException:     
         pass
-
-#AUTONAV Y SPEED CHALLENGE
-#3 CURSOS Y 6 RETOS 
-#FUTURO SUSCRIBIRSE A START Y STOP MISSION
-
-#Primero nos suscribimos
-
-
-
-#PARA MATAR SUBPROCESS 
-#TO 
-##self.course_pub = rospy.Publisher("/usv_comms/boat_transceiver/course_config", String, queue_size=10) SUSCRIBIRNOS
-#CALL BACK
-#
-#   DECODIFICAR SWICH CASE 
-#MAIN
-#  
 
